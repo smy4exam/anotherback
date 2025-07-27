9c62b54d820dd8faa44ae7613bfed15db5935b10
smy4exam <smy4exam@gmail.com>
2025-07-27 18:04:30 +0900

    initial commit & push

---
+++ src/main/java/com/example/backend/config/WebConfig.java
@@ -0,0 +1,19 @@
+package com.example.backend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://localhost:5173") // React app's URL
+               // .allowedOrigins("http://fullstack-frontend-app-bucket.s3-website.ap-northeast-2.amazonaws.com")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
